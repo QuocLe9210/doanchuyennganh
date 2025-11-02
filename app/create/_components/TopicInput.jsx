12c27e52794007429725685de909c2569a07b4c0
QuocLe9210 <leanhquoc9210@gmail.com>
2025-11-02 12:25:22 +0700

    update sửa topicinput trong create

--- app/create/_components/TopicInput.jsx
+++ app/create/_components/TopicInput.jsx
@@ -7,23 +7,25 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Stethoscope, StethoscopeIcon } from "lucide-react";
 
-function TopicInput({setTopic, setMứcđộcủakhóahọc}) {
+function TopicInput({ setTopic, setDifficultyLevel }) {
   return (
     <div className="mt-10 w-full flex flex-col">
       <h2>Nhập lựa chọn mong muốn để có thể khởi tạo khóa học</h2>
-      <Textarea placeholder="Nhập nội dung ở đây" 
-      className="mt-2 w-full" onChange = {(event)=>setTopic(event.target.value)} />
-      <h2 className="mt-5 mb-3">Nhập mức độ cho khóa học </h2>
+      <Textarea 
+        placeholder="Nhập nội dung ở đây" 
+        className="mt-2 w-full" 
+        onChange={(event) => setTopic(event.target.value)} 
+      />
+      <h2 className="mt-5 mb-3">Nhập mức độ cho khóa học</h2>
 
-      <Select onValueChange={(value)=>setMứcđộcủakhóahọc(value)}>
+      <Select onValueChange={(value) => setDifficultyLevel(value)}>
         <SelectTrigger className="w-full">
           <SelectValue placeholder="Mức độ của khóa học" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="de">Dễ </SelectItem>
-          <SelectItem value="binhthuong">Bình Thường </SelectItem>
+          <SelectItem value="de">Dễ</SelectItem>
+          <SelectItem value="binhthuong">Bình Thường</SelectItem>
           <SelectItem value="kho">Khó</SelectItem>
         </SelectContent>
       </Select>
